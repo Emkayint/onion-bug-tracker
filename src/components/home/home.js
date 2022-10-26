0a81b539b22277517454e314849a15e9da76c718
Martin <mkmartinoes@gmail.com>
2022-10-26 11:21:03 +0300

    st: layout - navbar design

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Navbar from "../navbar/Nabar";
 import "./home.css"
 
 export default function Home() {
   
   return (
-    <div className="container-main hero">
-      <div className="container-main row h-100">
-        <div className="col-sm-12 col-md-3 col-lg-2">
-          
+    <div className="container-main hero ">
+      <div className="row h-100">
+        <div className="col-sm-12 col-md-3 col-lg-2 h-100">
+          <Navbar />
         </div>
         <div className="col-sm-12 col-md-7 col-lg-10"></div>
       </div>
---
+++ src/components/navbar/Nabar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css"
+import  {BsBugFill,  BsLayersHalf, BsFlagFill} from "react-icons/bs" 
+
+export default function Navbar(){
+
+  return (
+    <div className="container-main Navbar h-100">
+      <header>
+        <h3>Tracker</h3>
+      </header>
+      <div className="container-main text-white px-4">
+        <br />
+        <p>My Space</p>
+      </div>
+      <hr className="bg-white" />
+
+      <div className="container-main px-4">
+        <NavLink to="/hello">
+          <p className="text-white">
+            <span className="pr-3">
+              <BsFlagFill />
+            </span>
+            Milestones
+          </p>
+        </NavLink>
+        <NavLink to="/hello">
+          <p className="text-white">
+            <span className="pr-3">
+              <BsBugFill />
+            </span>
+            Bugs
+          </p>
+        </NavLink>
+
+        <NavLink to="/hello">
+          <p className="text-white">
+            <span className="pr-3">
+              <BsLayersHalf />
+            </span>
+            Feed
+          </p>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
