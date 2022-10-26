54f34d57e1f24a25d9bca427b9bef82c8ce7a70d
Martin <mkmartinoes@gmail.com>
2022-10-26 22:22:49 +0300

    st: layout  allign bug card text details to center

--- src/components/dashboard/BugCard.js
+++ src/components/dashboard/BugCard.js
@@ -8,7 +8,7 @@ export default function BugCard(){
         </div>
         <div className="col-lg-3">
           <div className="bug-name">
-            <p>Create Company logo in 3 different Styles</p>
+            <p className="">Create Company logo in 3 different Styles</p>
             <p>
               <BsChatDots /> 43
             </p>
@@ -19,8 +19,12 @@ export default function BugCard(){
             <span className="btn btn-primary">Failed QA</span>
           </div>
         </div>
-        <div className="col-lg-2">03/04/22</div>
-        <div className="col-lg-2">Due in 5days</div>
+        <div className="col-lg-2">
+          <div className="date text-center">03/04/22</div>
+        </div>
+        <div className="col-lg-2">
+          <div className="due text-center">Due in 5days</div>
+        </div>
       </div>
     </div>
   );
